79bfdc789e61dbdc0d713faab903184fdfaad8f1
nishith-02 <sainishith02.varaganti@gmail.com>
2022-05-15 18:49:05 +0530

    cart page

---
+++ pages/orders/[id].js
@@ -0,0 +1,10 @@
+import styles from "../../styles/Order.module.css"
+import Image from "next/image"
+const Order=()=>{
+    return(
+        <div className={styles.container}>
+
+        </div>
+    )
+}
+export default Order
